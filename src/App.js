6322985f5dc8dd051e47a1de1cf1330b65773a45
Abhinav Yadav <abhiyadavpky@gmail.com>
2024-03-15 20:43:55 +0530

    done

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function Counter() {
             ? "Today is "
             : count > 0
             ? `${count} days from today is `
-            : `${count} days ago was `}
+            : `${Math.abs(count)} days ago was `}
         </span>
         <span>{date.toDateString()}</span>
       </p>
